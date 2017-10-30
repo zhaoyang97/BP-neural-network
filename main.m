2c38ae3aba716ad7bde8cbc8cde94978a4263178
fly2core <1173201671@qq.com>
2017-10-30 12:22:45 +0800

    修复绘图曲线图例显示bug
    当输入样本不小于4时，显示前四条曲线样例，小于4时不现实。

--- main.m
+++ main.m
@@ -12,7 +12,9 @@ end
 
 % 绘图
 plot(result);
-legend(num2str(X(1,:)),num2str(X(2,:)),num2str(X(3,:)),num2str(X(4,:)));
+if size(X, 1)>=4
+    legend(num2str(X(1,:)),num2str(X(2,:)),num2str(X(3,:)),num2str(X(4,:)));
+end
 title('异或');
 xlabel('训练次数');
 ylabel('为1的概率，即不为0的概率');
